1d4cb4d95de6c82257a1a14ac8e469d008d78e9e
yusuke0402 <nakyu0402@gmail.com>
2025-08-15 14:45:48 +0900

    ATE推定用IPW実装、二つのソースを一つにまとめた

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import yaml
 import random
 from data import DataSets
 from propensityscore import propensityscore
-from wasserstain_distance import wasserstein_distance
-from weight import weight_calculation
+
 
 
 
@@ -20,32 +19,23 @@ for i in range(0,config["hyperparam"]["n_trial"]):
     random.seed(i)
     
     data=DataSets()
-
+    integrated_historical_x=np.concatenate((data.training_historical_1_x,data.training_historical_2_x),axis=0)
+    integrated_historical_y=np.concatenate((data.training_historical_1_y,data.training_historical_2_y),axis=0)
     #2.傾向スコアの推定
-    ppscore_target_1,ppscore_source1 = propensityscore(target_x=data.training_current_x[:,1:],target_y=data.training_current_y,source_x=data.training_historical_1_x[:,1:],source_y=data.training_historical_1_y)
-    ppscore_target_2,ppscore_source2 = propensityscore(target_x=data.training_current_x[:,1:],target_y=data.training_current_y,source_x=data.training_historical_2_x[:,1:],source_y=data.training_historical_2_y)
+    ppscore_target,ppscore_source = propensityscore(target_x=data.training_current_x[:,1:],target_y=data.training_current_y,source_x=integrated_historical_x[:,1:],source_y=integrated_historical_y)
     
-    #3.wasserstain_distanceの計算
-    wasserstein_dist_1 = wasserstein_distance(data.training_current_x[:,1:], data.training_historical_1_x[:,1:])
-    wasserstein_dist_2 = wasserstein_distance(data.training_current_x[:,1:], data.training_historical_2_x[:,1:])
-    #4.重みの計算
-    weight_1=wasserstein_dist_2*weight_calculation(ppscore_source1, wasserstein_dist_1, wasserstein_dist_2)
-    weight_2=wasserstein_dist_1*weight_calculation(ppscore_source2, wasserstein_dist_1, wasserstein_dist_2)
-    #5.推定値の計算
-    result[i] = np.mean(data.training_current_y)-(weight_1@data.training_historical_1_y).item()-(weight_2@data.training_historical_2_y).item()
-    print((weight_1@data.training_historical_1_y).item()) 
-    print(np.mean(data.training_historical_1_y))
-    print(wasserstein_dist_2/(wasserstein_dist_1 + wasserstein_dist_2))
-    inv_ppscore = 1 / ppscore_source1
-    sum_inv_ppscore = np.sum(inv_ppscore)
-    print(np.sum(wasserstein_dist_2*inv_ppscore/sum_inv_ppscore/(wasserstein_dist_1 + wasserstein_dist_2)))
-    print((inv_ppscore/sum_inv_ppscore)@data.training_historical_1_y)
-    print(result[i])
-    print(np.mean(data.training_current_y))
-    print((weight_2@data.training_historical_2_y).item())
+    #3.重みの計算
+    weight_source=1/(1-ppscore_source)
+    weight_target=1/(ppscore_target)
     
+    #4.推定値の計算
+    result[i]=np.sum(weight_target*data.training_current_y.ravel()) /np.sum(weight_target)-np.mean(weight_source@integrated_historical_y.ravel())/np.sum(weight_source)
     
-    
+print("mean：",np.mean(result))
+print("MSE：",np.mean((result-true_values)**2))
+print("bias：",np.mean(result-true_values))
+print("variance：",np.var(result))
+print("sd：",np.std(result))
 
 
 
--- weight.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def weight_calculation(ppscore,wasserstein_dist_1, wasserstein_dist_2):
-    inv_ppscore = 1 / ppscore
-    sum_inv_ppscore = np.sum(inv_ppscore)
-    return inv_ppscore/sum_inv_ppscore/(wasserstein_dist_1 + wasserstein_dist_2)
