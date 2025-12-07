022fa1a9d5f05958d9a585fb984d2c9fb7671bef
yusuke_mac <nakyu0402@gmail.com>
2025-12-07 18:12:59 +0900

    :sparkles: change the way of saving data

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
+import pandas as pd
 import yaml
 import random
+import os
+import datetime
 from data import DataSets
 from propensityscore import propensityscore
 from wasserstain_distance import wasserstein_distance
@@ -12,8 +15,8 @@ from weight import weight_calculation
 with open("configs/config.yaml", "r") as f:
     config = yaml.safe_load(f)
 split=config["hyperparam"]["n_split"]
-result=np.empty(config["hyperparam"]["n_trial"])
-true_values=np.ones_like(result)
+results=[]
+
 for i in range(0,config["hyperparam"]["n_trial"]):
     #1.データ作成
     np.random.seed(i)
@@ -29,20 +32,59 @@ for i in range(0,config["hyperparam"]["n_trial"]):
     wasserstein_dist_1 = wasserstein_distance(data.training_current_x[:,1:], data.training_historical_1_x[:,1:])
     wasserstein_dist_2 = wasserstein_distance(data.training_current_x[:,1:], data.training_historical_2_x[:,1:])
     #4.重みの計算
-    weight_1=wasserstein_dist_2*weight_calculation(ppscore_source1, wasserstein_dist_1, wasserstein_dist_2)
-    weight_2=wasserstein_dist_1*weight_calculation(ppscore_source2, wasserstein_dist_1, wasserstein_dist_2)
+    weight_source_1=1/(1-ppscore_source1)
+    weight_source_2=1/(1-ppscore_source2)
+    weight_target_1=1/(ppscore_target_1)  
+    weight_target_2=1/(ppscore_target_2)
+    weight_mean_1=wasserstein_dist_2/(wasserstein_dist_1+wasserstein_dist_2)
+    weight_mean_2=wasserstein_dist_1/(wasserstein_dist_1+wasserstein_dist_2)
     #5.推定値の計算
-    result[i] = np.mean(data.training_current_y)-(weight_1@data.training_historical_1_y).item()-(weight_2@data.training_historical_2_y).item()
-    print((weight_1@data.training_historical_1_y).item()) 
-    print(np.mean(data.training_historical_1_y))
-    print(wasserstein_dist_2/(wasserstein_dist_1 + wasserstein_dist_2))
-    inv_ppscore = 1 / ppscore_source1
-    sum_inv_ppscore = np.sum(inv_ppscore)
-    print(np.sum(wasserstein_dist_2*inv_ppscore/sum_inv_ppscore/(wasserstein_dist_1 + wasserstein_dist_2)))
-    print((inv_ppscore/sum_inv_ppscore)@data.training_historical_1_y)
-    print(result[i])
-    print(np.mean(data.training_current_y))
-    print((weight_2@data.training_historical_2_y).item())
+    mean_1= np.sum(weight_target_1 * data.training_current_y.ravel())/np.sum(weight_target_1)-np.sum(weight_source_1 * data.training_historical_1_y.ravel()) / np.sum(weight_source_1)
+    mean_2= np.sum(weight_target_2 * data.training_current_y.ravel())/np.sum(weight_target_2)-np.sum(weight_source_2 * data.training_historical_2_y.ravel()) / np.sum(weight_source_2)
+    estimate= mean_1 * weight_mean_1 + mean_2 * weight_mean_2
+    
+    results.append({
+        "trial" : i+1,
+        "estimate_value" : estimate,
+        "weight_source_1" :weight_mean_1,
+        "weight_source_2" : weight_mean_2,
+        "source_1_mean" : mean_1,
+        "source_2_mean" : mean_2
+    
+    })
+
+# 結果の保存
+now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+if not os.path.exists("results"):
+    os.makedirs("results")
+df = pd.DataFrame(results)
+df.to_csv(f"results/result_{now}.csv", index=False)
+print(f"詳細ログを保存しました: results/result_{now}.csv")
+# 統計量の計算
+estimates = df["estimate_value"]
+
+stats = {
+    "mean": float(estimates.mean()),
+    "variance": float(estimates.var()),
+    "std_dev": float(estimates.std()),
+    "mse": float(np.mean((estimates - config["datasettings"]["true_value"]) ** 2)),
+}
+
+summary_data = {
+    "timestamp": now,
+    "senario_name": config["senario"]["pattern_id"],
+    "current_number": config["datasettings"]["target_number"],
+    "historical_1_number": config["datasettings"]["source1_number"],
+    "historical_2_number": config["datasettings"]["source2_number"],
+    "n_trial": config["hyperparam"]["n_trial"],
+    "statistics": stats,
+    "notes": "YOU CAN WRITE SOME NOTES HERE",
+}
+with open(f"results/summary_{now}.yaml", "w", encoding="utf-8") as f:
+    yaml.safe_dump(
+        summary_data, f, default_flow_style=False, sort_keys=False, allow_unicode=True
+    )
+print(f"統計量を保存しました: results/summary_{now}.yaml")
     
     
     
