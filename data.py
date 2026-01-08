878fc194164225853485fd50c022b48b4bb76251
yusuke_mac <nakyu0402@gmail.com>
2026-01-08 17:44:51 +0900

    :sparkles: feat:make dimensionality configurable

--- data.py
+++ data.py
@@ -1,77 +1,75 @@
 import numpy as np
 import pandas as pd
 import yaml
-from model_factory import ModelFactory
+
 class DataSets:
 
-  with open("configs/config.yaml", "r") as f:
-    config = yaml.safe_load(f)
-
-  target_split = int(config["hyperparameters"]["split_ratio"] * config["dataset"]["target_number"])
+  
 
  #初期化
-  def __init__(self):    
-    data_scenario_id=DataSets.config["scenario"]["data_scenario_id"]
-    n_features= DataSets.config["hyperparameters"]["n_features"]
-    mean_df=pd.read_csv(f"configs/{n_features}dim_means.csv")
-    cov_df=pd.read_csv(f"configs/{n_features}dim_covariances.csv")
+  def __init__(self,config):    
+    self.data_scenario_id=config["scenario"]["data_scenario_id"]
+    self.n_features=config["hyperparameters"]["n_features"]
+    self.mean_df=pd.read_csv(f"configs/{self.n_features}dim_means.csv")
+    self.cov_df=pd.read_csv(f"configs/{self.n_features}dim_covariances.csv")
+    self.coefficient_df=pd.read_csv("configs/coefficient.csv")
+    self.__load_means_and_covariances()
+    self.config=config
     
-
-    def generate_multivariate_normal(domain, size=1):
+  def __generate_multivariate_normal(self, domain, means, covariances, size=1):
       mean_vector = means[domain]  # 平均ベクトルを1次元配列として生成
       cov_matrix = covariances[domain] #分散共分散行列
       x=np.random.multivariate_normal(mean_vector, cov_matrix, size)
-      return np.insert(x,0,1,axis=1),mean_vector
-
+      return np.insert(x,0,1,axis=1)
+  def __load_means_and_covariances(self):
     means={
-    row['domain']: np.array([row[f'dim{i+1}'] for i in range(n_features)])
-      for _, row in mean_df.iterrows() if row['data_scenario_id']==data_scenario_id
+    row['domain']: np.array([row[f'dim{i+1}'] for i in range(self.n_features)])
+      for _, row in self.mean_df.iterrows() if row['data_scenario_id']==self.data_scenario_id
   } 
     covariances={}
-    for _, row in cov_df.iterrows():
-      if row['data_scenario_id']!=data_scenario_id:
+    for _, row in self.cov_df.iterrows():
+      if row['data_scenario_id']!=self.data_scenario_id:
         continue
       flat=np.array([float(x) for x in row['cov'].split(',')])
-      cov=flat.reshape((n_features,n_features))
+      cov=flat.reshape((self.n_features,self.n_features))
       covariances[row['domain']]=cov
-
-
-    self.__target_number=DataSets.config["dataset"]["target_number"] #現在試験の被験者数
-    self.__target_x, self.__target_x_mean=generate_multivariate_normal("target",size=self.__target_number) #現在試験の共変量　行が被験者、列が共変量
+    self.means=means
+    self.covariances=covariances
+  def __get_output(self,input,t,epsilon):
+        if self.config["scenario"]["model_id"] == "linear":
+            coefficient_row=self.coefficient_df[(self.coefficient_df["n_features"]==self.config["hyperparameters"]["n_features"]) & (self.coefficient_df["model_scenario_id"]==self.config["scenario"]["model_scenario_id"])]
+            if coefficient_row.empty:
+                raise ValueError("Coefficient not found for the specified model_id, n_features, and scenario_id.")
+            coefficient = np.array([float(x) for x in coefficient_row.iloc[0]["coefficient"].split(",")])
+            output = coefficient @ input.T + t + epsilon
+            return np.array([output]).T
+        else:
+            raise ValueError(f"Unknown model_id: {self.config['scenario']['model_id']}")
+  def generate_data(self,):
+    self.__target_number=self.config["dataset"]["target_number"] #現在試験の被験者数
+    self.__target_x=self.__generate_multivariate_normal(domain="target",means=self.means,covariances=self.covariances,size=self.__target_number) #現在試験の共変量　行が被験者、列が共変量
     self.__epsilon=np.random.normal(loc=0,scale=1,size=self.__target_number) #測定誤差
-    self.__target_y=ModelFactory.get_output(
+    self.__target_y=self.__get_output(
             input=self.__target_x, t=np.ones(self.__target_number), epsilon=self.__epsilon
     )
 
-    self.__source_number = DataSets.config["dataset"]["source_number"]
-    self.__source_x, self.__source_x_mean=generate_multivariate_normal("source",size=self.__source_number)
+    self.__source_number = self.config["dataset"]["source_number"]
+    self.__source_x=self.__generate_multivariate_normal("source",self.means,self.covariances,size=self.__source_number)
     self.__epsilon=np.random.normal(loc=0,scale=1,size=self.__source_number)
-    self.__source_y=ModelFactory.get_output(
+    self.__source_y=self.__get_output(
             input=self.__source_x, t=np.zeros(self.__source_number), epsilon=self.__epsilon
         )
-  
+
 #変数のカプセル化、意図しない値の書き換えを防ぐ目的
   @property
-  def training_target_x(self):
-     return self.__target_x[0:self.target_split,:]
+  def target_x(self):
+     return self.__target_x
   @property
-  def verifying_target_x(self):
-     return self.__target_x[self.target_split:,:]
+  def target_y(self):
+     return self.__target_y
   @property
-  def training_target_y(self):
-     return self.__target_y[0:self.target_split,:]
-  @property
-  def verifying_target_y(self):
-     return self.__target_y[self.target_split:,:]
-  @property
-  def training_source_x(self):
+  def source_x(self):
     return self.__source_x
   @property
-  def training_source_y(self):
+  def source_y(self):
     return self.__source_y
-  @property
-  def current_x_mean(self):
-    return self.__target_x_mean
-  @property
-  def source_x_mean(self):
-    return self.__source_x_mean
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import os
 import datetime
 from data import DataSets
 from propensityscore import propensityscore
+from result import Result
 
 
 
@@ -14,67 +15,35 @@ with open("configs/config.yaml", "r") as f:
     config = yaml.safe_load(f)
 split=config["hyperparameters"]["n_split"]
 results=[]
+n_trial=config["hyperparameters"]["n_trial"]
 
-for i in range(0,config["hyperparameters"]["n_trial"]):
-    #1.データ作成
+for i in range(0,n_trial):
     np.random.seed(i)
     random.seed(i)
-    
-    data=DataSets()
-
+    #1.データの生成
+    data=DataSets(config=config)
+    data.generate_data()
+    target_x=data.target_x
+    source_x=data.source_x
+    target_y=data.target_y
+    source_y=data.source_y
     #2.傾向スコアの推定
-    ppscore_target,ppscore_source = propensityscore(target_x=data.training_target_x[:,1:],target_y=data.training_target_y,source_x=data.training_source_x[:,1:],source_y=data.training_source_y)
-    
-    #4.重みの計算
+    ppscore_target,ppscore_source = propensityscore(target_x=target_x[:,1:],target_y=target_y,source_x=source_x[:,1:],source_y=source_y)
+
+    #3.重みの計算
     weight_source=1/(1-ppscore_source)
     weight_target=1/(ppscore_target)
-    #5.推定値の計算
-    mean= np.sum(weight_target * data.training_target_y.ravel())/np.sum(weight_target)-np.sum(weight_source * data.training_source_y.ravel()) / np.sum(weight_source)
+    #4.推定値の計算
+    mean= np.sum(weight_target * target_y.ravel())/np.sum(weight_target)-np.sum(weight_source * source_y.ravel()) / np.sum(weight_source)
     estimate= mean
     results.append({
         "trial" : i+1,
         "estimate_value" : estimate
     })
 
-# 結果の保存
-now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-if not os.path.exists("results"):
-    os.makedirs("results")
-df = pd.DataFrame(results)
-df.to_csv(f"results/result_{now}.csv", index=False)
-print(f"詳細ログを保存しました: results/result_{now}.csv")
-# 統計量の計算
-estimates = df["estimate_value"]
-
-stats = {
-    "mean": float(estimates.mean()),
-    "variance": float(estimates.var()),
-    "std_dev": float(estimates.std()),
-    "mse": float(np.mean((estimates - config["hyperparameters"]["true_value"]) ** 2)),
-}
-
-summary_data = {
-    "timestamp": now,
-    "senario_name": config["scenario"]["data_scenario_id"],
-    "model_id":config["scenario"]["model_id"],
-    "model_scenario_id":config["scenario"]["model_scenario_id"],
-    "n_features":config["hyperparameters"]["n_features"],
-    "target_number": config["dataset"]["target_number"],
-    "source_number": config["dataset"]["source_number"],
-    "n_trial": config["hyperparameters"]["n_trial"],
-    "statistics": stats,
-    "notes": "YOU CAN WRITE SOME NOTES HERE",
-}
-with open(f"results/summary_{now}.yaml", "w", encoding="utf-8") as f:
-    yaml.safe_dump(
-        summary_data, f, default_flow_style=False, sort_keys=False, allow_unicode=True
-    )
-print(f"統計量を保存しました: results/summary_{now}.yaml")
+#5.結果の保存
     
-    
-    
-
-
+Result.save_results(results, config)
 
 
 
